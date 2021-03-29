8950093017a3e86afa9df7d2f458085e68acdf7d
IsidoroAros <60631478+IsidoroAros@users.noreply.github.com>
2021-03-29 13:02:17 -0300

    Plan

--- Actividad 1.3 Blueprint/Blueprint create.sql
+++ Actividad 1.3 Blueprint/Blueprint create.sql
@@ -1,37 +1,3 @@
-/*
-
-
-Realizar un modelo lógico en papel en el cual se identifiquen todas las entidades y cómo se relacionan entre sí.
-Realizar un script en lenguaje TSQL que genere la siguiente base de datos con sus respectivas tablas y restricciones.
-
-
-
-Blueprint será un software dedicado a la gestión de proyectos a medida de todo tipo. En él, 
-los administradores de proyectos serán capaces de poder registrar los proyectos, clientes, módulos, tareas, 
-colaboradores y demás elementos necesarios para la gestión de proyectos.
-
-En la primera etapa del desarrollo, el sistema requiere una base de datos en SQL Server Express que permita registrar 
-la información de los clientes y los proyectos. 
-
-El diseño de la base de datos debe garantizar que:
-Un cliente pueda tener un código autonumérico que lo identifique.
-Un cliente deba tener una razón social y un CUIT. Este último no puede repetirse.
-Un cliente deba registrar un tipo de cliente y que el gestor de proyecto pueda definir los distintos tipos de clientes: "Estatal", 
-	"Multinacional", "Educativo privado", "Educativo público", "Sin fines de lucro", etc.
-Un cliente pueda registrar un mail, teléfono y celular de contacto.
-Un proyecto registre un nombre y pueda opcionalmente registrar una descripción larga.
-Un proyecto registre un ID que lo identifique.
-Un proyecto debe tener un cliente.
-Un cliente puede tener muchos proyectos.
-Dos o más clientes no pueden tener el mismo proyecto.
-Un proyecto debe contener una fecha de inicio y una fecha de fin. Aunque esta última debe poder suministrarse tiempo después de haber registrado el proyecto.
-Un proyecto debe registrar un costo estimado.
-Un proyecto debe contener una marca que defina si se encuentra vigente o cancelado.
-
-A continuación se detalla un extracto de una planilla de cálculo con un ejemplo de la información que se registraría de cada elemento.
-
-*/
-
 Create database Blueprint
 GO
 use Blueprint
